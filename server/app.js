5850082b6910ce4a1e79f4e6a90fd6c7ac095b8c
EC2 Default User <ec2-user@ip-172-31-47-13.ec2.internal>
2019-10-31 06:18:35 +0000

    added some stuff, still confused about other stuff

---
+++ server/app.js
@@ -0,0 +1,42 @@
+
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const cors = require('cors');
+require('dotenv').config()
+
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+
+const loginRouter = require('./routes/login')
+const callbackRouter = require('./routes/callback')
+const refreshRouter = require('./routes/refresh')
+const metricsRouter = require('./routes/metrics')
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static('./src'))
+   .use(cors())
+   .use(cookieParser());
+
+app.get('/home', function(req, res) {
+  res.sendFile('views/home.html', {root: 'src'})
+})
+
+app.get('/apitest', function(req, res){
+  res.sendFile('views/apitest.html', {root: 'src'})
+})
+
+app.get('/testdata', function(req, res){
+  res.send({msg: 'from the server'})
+})
+
+
+loginRouter(app)
+callbackRouter(app)
+refreshRouter(app)
+metricsRouter(app)
+console.log('Listening on 3000');
+console.log(process.env.REDIRECT_URI)
+app.listen(3000);
---
+++ server/routes/metrics.js
@@ -0,0 +1,75 @@
+const request = require('request')
+
+module.exports = app => {
+	app.get('/top-artists', function(req, res){
+		
+		console.log('q', req.cookies)
+		var options = {
+      url: 'https://api.spotify.com/v1/me/top/artists',
+      headers: { 'Authorization': 'Bearer ' + req.cookies.access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, function(error, response, body) {
+      console.log('err',error)
+      console.log('res',response.statusCode)
+      console.log('bod',body);
+      res.send(body)
+    });
+	})
+
+	app.get('/top-tracks', function(req, res){
+		
+		console.log('q', req.cookies)
+		var options = {
+      url: 'https://api.spotify.com/v1/me/top/tracks',
+      headers: { 'Authorization': 'Bearer ' + req.cookies.access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, function(error, response, body) {
+      console.log('err',error)
+      console.log('res',response.statusCode)
+      console.log('bod',body);
+      res.send(body)
+    });
+	})
+}
+app.get('recommendations', function(req, res){
+    
+    console.log('q', req.cookies)
+    var options = {
+      url: 'https://api.spotify.com/v1/recommendations?limit=10&seed_genres=hip-hop&min_acousticness=0&max_acousticness=50&target_acousticness=20&min_danceability=20&max_danceability=100&target_danceability=70&min_popularity=50&max_popularity=100&target_popularity=80',
+      headers: { 'Authorization': 'Bearer ' + req.cookies.access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, function(error, response, body) {
+      console.log('err',error)
+      console.log('res',response.statusCode)
+      console.log('bod',body);
+      res.send(body)
+    });
+  })
+}
+app.get('recommendations', function(req, res){
+    
+    console.log('q', req.cookies)
+    var options = {
+      url: 'https://api.spotify.com/v1/browse/featured-playlists?country=US&limit=10&offset=5',
+      headers: { 'Authorization': 'Bearer ' + req.cookies.access_token },
+      json: true
+    };
+
+    // use the access token to access the Spotify Web API
+    request.get(options, function(error, response, body) {
+      console.log('err',error)
+      console.log('res',response.statusCode)
+      console.log('bod',body);
+      res.send(body)
+    });
+  })
+}
